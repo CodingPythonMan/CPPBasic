f2d6d7d5c69ec5fa1956f4637873f8bb61cf3a92
JuYoung Kim <robolab1902@naver.com>
2022-12-27 11:13:54 +0900

    기존에 있던 Window 도 메세지를 통해 건들일 수 있고 IPC를 하기 위한 사용자 정의 메세지도 숙지하였다

---
+++ WSProgramming1/Section02/Handle/findwindow2.cpp
@@ -0,0 +1,14 @@
+/*#include <stdio.h>
+#include <Windows.h>
+#include <tchar.h>
+
+int main()
+{
+	HWND hwnd = FindWindow(0, _T("제목 없음 - Windows 메모장"));
+
+	printf("%d\n", hwnd);
+
+	getchar(); 
+
+	DestroyWindow(hwnd);
+}*/
---
+++ WSProgramming1/Section02/Handle/handle1.cpp
@@ -0,0 +1,16 @@
+/*typedef int HWND;
+typedef int HICON;
+
+void MoveWindow(HWND hwnd, int x, int y)
+{
+
+}
+
+HICON CreateIcon() { return 0; }
+
+int main()
+{
+	HICON hIcon = CreateIcon();
+
+	MoveWindow(hIcon, 10, 10);
+}*/
